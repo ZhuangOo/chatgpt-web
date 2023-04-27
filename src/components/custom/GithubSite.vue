48f3308789bd564fa2858decbe2ab8cb51833335
ZhuangOo <80512517+ZhuangOo@users.noreply.github.com>
2023-04-27 14:59:38 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="text-neutral-400">
     <span>Hello</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      !
-    </a>
+    
   </div>
 </template>
