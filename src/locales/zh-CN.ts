17b7215def5aeb57dcdc2a30c278fafbd085d746
ZhuangOo <80512517+ZhuangOo@users.noreply.github.com>
2023-04-27 22:05:32 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -29,7 +29,7 @@ export default {
   },
   chat: {
     newChatButton: '新建聊天',
-    placeholder: '来说点什么吧...（Shift + Enter = 换行，"/" 触发提示词）',
+    placeholder: '来说点什么吧...（Shift + Enter = 换行，"/" 显示输入记录）',
     placeholderMobile: '来说点什么...',
     copy: '复制',
     copied: '复制成功',
@@ -74,7 +74,7 @@ export default {
     monthlyUsage: '本月使用量',
   },
   store: {
-    siderButton: '搜索记录',
+    siderButton: '输入记录',
     local: '本地',
     online: '在线',
     title: '标题',
